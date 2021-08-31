3e0addc3cb2dd390be91173c6e5e3e2b1ddfd96c
leushinilya <leushinilya@gmail.com>
2021-09-01 00:17:49 +0300

    navigation added

--- app/src/main/java/com/leushinilya/loftcoin/ui/main/MainActivity.java
+++ app/src/main/java/com/leushinilya/loftcoin/ui/main/MainActivity.java
@@ -1,16 +1,29 @@
 package com.leushinilya.loftcoin.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.leushinilya.loftcoin.R;
+import com.leushinilya.loftcoin.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        final ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavController controller = Navigation.findNavController(this, R.id.main_host);
+        NavigationUI.setupWithNavController(binding.toolbar, controller, new AppBarConfiguration
+                .Builder(binding.bottomNav.getMenu())
+                .build());
+        NavigationUI.setupWithNavController(binding.bottomNav, controller);
+
     }
 }
